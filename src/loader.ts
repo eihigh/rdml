dcd722ff486d141e59c82658465e7273f228d2a2
eihigh <eihigh.contact@gmail.com>
2019-01-22 18:54:37 -0800

    loader: add

---
+++ src/loader.ts
@@ -0,0 +1,64 @@
+/// <reference path="rdml.ts" />
+
+namespace rdml {
+
+  export function load(path: string) {
+    if (!(path in files)) {
+      files[path] = new File(path);
+    }
+  }
+
+  export function hasLoaded() {
+    let loaded = true;
+    for (const path in files) {
+      const file = files[path];
+      loaded = loaded && file.loaded;
+    }
+    return loaded;
+  }
+
+  let files: { [path: string]: File } = {};
+
+  class File {
+    xhr: XMLHttpRequest = new XMLHttpRequest();
+    loaded: boolean = false;
+
+    constructor(private path: string) {
+      const fullpath = "rdml/" + path;
+      this.xhr.open("GET", fullpath);
+      this.xhr.onload = () => {
+        if (this.xhr.status < 400) {
+          this.onload();
+          this.loaded = true;
+        }
+        // TODO: error handling
+      };
+      this.xhr.send();
+    }
+
+    onload() {
+      const nodes: Node[] = [];
+      let pname = ""; // package name
+
+      for (const node of nodes) {
+        if (typeof node === "string") { continue; }
+
+        const elem: Element = node;
+        if (elem.name === "package") {
+          const pname = elem.attrs["name"];
+          for (const child of elem.children) {
+            this.make(pname, child);
+          }
+        }
+
+        this.make("", elem);
+      }
+    }
+
+    make(pname: string, elem: Element) {
+      switch (elem.name) {
+        case "proc":
+      }
+    }
+  }
+}
